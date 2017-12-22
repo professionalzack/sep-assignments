6d703001c0f0c44f9ebffd018f149150241c9678
zack <zack.etc@gmail.com>
2017-12-22 15:57:12 -0800

    myqueue

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -8,11 +8,25 @@ class MyQueue
   end
 
   def enqueue(element)
+    @queue << element
+    @head = @queue[0]
+    @tail = @queue[@queue.length-1]
+
   end
 
   def dequeue
+    nextUp = @head
+    @queue.delete(head)
+    @head = @queue[0]
+    @tail = @queue[@queue.length-1]
+
   end
 
   def empty?
+    if @queue.length === 0 
+      return true
+    else
+      return false
+    end
   end
 end
